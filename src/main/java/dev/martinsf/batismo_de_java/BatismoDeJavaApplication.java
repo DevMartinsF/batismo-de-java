bccef5c8bb6fc169affe7e62ef7cfeb79c93b993
DevMartinsF <r.martins.programmer@gmail.com>
2026-02-17 18:20:55 -0300

    chore: initial commit

---
+++ src/main/java/dev/martinsf/batismo_de_java/BatismoDeJavaApplication.java
@@ -0,0 +1,13 @@
+package dev.martinsf.batismo_de_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatismoDeJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatismoDeJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/martinsf/batismo_de_java/BatismoDeJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.martinsf.batismo_de_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatismoDeJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
