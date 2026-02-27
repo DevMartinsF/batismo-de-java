06cb2441075b343212a1bacc3261d8d072485502
DevMartinsF <r.martins.programmer@gmail.com>
2026-02-27 16:55:35 -0300

    refactor: substitui construtores, getters e setters das entidades por anotações do Lombok

--- src/main/java/dev/martinsf/batismo_de_java/Missoes/MissoesModel.java
+++ src/main/java/dev/martinsf/batismo_de_java/Missoes/MissoesModel.java
@@ -2,11 +2,15 @@ package dev.martinsf.batismo_de_java.Missoes;
 
 import dev.martinsf.batismo_de_java.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 import java.util.List;
 
 @Entity
 @Table (name = "tb_missoes")
+@AllArgsConstructor
+@Data
 public class MissoesModel {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
@@ -15,42 +19,4 @@ public class MissoesModel {
     private RankDeMissoes rank;
     @OneToMany (mappedBy = "missao")
     private List<NinjaModel> ninjasDaMissao;
-
-    public MissoesModel(Long id, String nome, RankDeMissoes rank, List<NinjaModel> ninjasDaMissao) {
-        this.id = id;
-        this.nome = nome;
-        this.rank = rank;
-        this.ninjasDaMissao = ninjasDaMissao;
-    }
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public List<NinjaModel> getNinjasDaMissao() {
-        return ninjasDaMissao;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public RankDeMissoes getRank() {
-        return rank;
-    }
-
-    public void setRank(RankDeMissoes rank) {
-        this.rank = rank;
-    }
-
-    public void setNinjasDaMissao(List<NinjaModel> ninjasDaMissao) {
-        this.ninjasDaMissao = ninjasDaMissao;
-    }
 }
--- src/main/java/dev/martinsf/batismo_de_java/Ninjas/NinjaModel.java
+++ src/main/java/dev/martinsf/batismo_de_java/Ninjas/NinjaModel.java
@@ -2,9 +2,14 @@ package dev.martinsf.batismo_de_java.Ninjas;
 
 import dev.martinsf.batismo_de_java.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table (name = "tb_cadastro")
+@AllArgsConstructor
+@Data
 public class NinjaModel {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
@@ -17,43 +22,4 @@ public class NinjaModel {
     @ManyToOne
     @JoinColumn (name = "missao_id")
     private MissoesModel missao;
-
-    public NinjaModel(Long id, String nome, int idade, String gmail) {
-        this.id = id;
-        this.nome = nome;
-        this.idade = idade;
-        this.gmail = gmail;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public String getGmail() {
-        return gmail;
-    }
-
-    public void setGmail(String gmail) {
-        this.gmail = gmail;
-    }
 }
