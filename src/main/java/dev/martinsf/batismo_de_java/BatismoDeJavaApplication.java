0919fefa77ce1e11478f03071900994f30bc03e3
DevMartinsF <r.martins.programmer@gmail.com>
2026-02-28 15:56:39 -0300

    fix: concerta a exposição das credencias do H2 e adiciona as variaveis de ambiente

--- src/main/java/dev/martinsf/batismo_de_java/BatismoDeJavaApplication.java
+++ src/main/java/dev/martinsf/batismo_de_java/BatismoDeJavaApplication.java
@@ -1,4 +1,4 @@
-package dev.martinsf.batismo_de_java;
+package dev.martinsf.batismo_de_java;xg
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
