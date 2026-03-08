17f0da3a49d0d9e338309367cf61b446b499ec8f
DevMartinsF <r.martins.programmer@gmail.com>
2026-03-08 11:04:20 -0300

    Fix: extende JpaRepository e muda de clase para interface o NinjaRepository

--- src/main/java/dev/martinsf/batismo_de_java/Ninjas/NinjaRepository.java
+++ src/main/java/dev/martinsf/batismo_de_java/Ninjas/NinjaRepository.java
@@ -1,4 +1,6 @@
 package dev.martinsf.batismo_de_java.Ninjas;
 
-public class NinjaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
 }
