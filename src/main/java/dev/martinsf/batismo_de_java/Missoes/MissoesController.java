69a1ec015ed20d2aefd4132335f965c9ccb24e7a
DevMartinsF <r.martins.programmer@gmail.com>
2026-03-08 12:24:56 -0300

    feat: injeta o Missoesservice no Missoescontroller e implementa a logica no metodo para mostrar as missoes

--- src/main/java/dev/martinsf/batismo_de_java/Missoes/MissoesController.java
+++ src/main/java/dev/martinsf/batismo_de_java/Missoes/MissoesController.java
@@ -2,17 +2,24 @@ package dev.martinsf.batismo_de_java.Missoes;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping ("/missoes")
 public class MissoesController {
+    private MissoesService missoesService;
+
+    public MissoesController(MissoesService missoesService) {
+        this.missoesService = missoesService;
+    }
 
     @PostMapping ("/adicionarMissao")
     public String adicionarMissao(){
         return "Missao adicionada";
     }
     @GetMapping ("/mostrarMissoes")
-    public String mostrarTodasAsMissoes() {
-    return "Todas Missoes";
+    public List <MissoesModel> mostrarTodasAsMissoes() {
+    return missoesService.mostrarMissoes();
     }
     @GetMapping ("/mostarMissaoID")
     public String mostrarMissaoId() {
