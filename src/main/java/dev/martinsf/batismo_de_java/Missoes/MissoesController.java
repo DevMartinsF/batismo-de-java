98130c3cc605d7059421bae1e77f0543cae26ee9
DevMartinsF <r.martins.programmer@gmail.com>
2026-03-07 21:09:55 -0300

    feat: atualiza MissoesController com mock de dados

--- src/main/java/dev/martinsf/batismo_de_java/Missoes/MissoesController.java
+++ src/main/java/dev/martinsf/batismo_de_java/Missoes/MissoesController.java
@@ -1,4 +1,28 @@
 package dev.martinsf.batismo_de_java.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
 public class MissoesController {
+
+    @PostMapping (name = "/adicionarmissao")
+    public String adicionarMissao(){
+        return "Missao adicionada";
+    }
+    @GetMapping (name = "/mostrarmissoes")
+    public String mostrarTodasAsMissoes() {
+    return "Todas Missoes";
+    }
+    @GetMapping (name = "/mostarmissaoid")
+
+    @PutMapping ("/atualizarM")
+    public String atualizarMissao (){
+        return "Missao atualizada";
+    }
+
+    @DeleteMapping (name = "/deletarM")
+    public String deletarMissao(){
+        return "Missao Deletada";
+    }
 }
