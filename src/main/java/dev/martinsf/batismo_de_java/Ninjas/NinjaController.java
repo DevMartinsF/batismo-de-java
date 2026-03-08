c2f5d39995529e1a4e75e26c4914b62fd5becda6
DevMartinsF <r.martins.programmer@gmail.com>
2026-03-07 21:01:17 -0300

    feat: atualiza o NinjaController com mock de dados

--- src/main/java/dev/martinsf/batismo_de_java/Ninjas/NinjaController.java
+++ src/main/java/dev/martinsf/batismo_de_java/Ninjas/NinjaController.java
@@ -1,13 +1,36 @@
 package dev.martinsf.batismo_de_java.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
 @RequestMapping
 public class NinjaController {
     @GetMapping("/boasvindas")
     public String boasVindas (){
         return "Boas Vindas";
+    }
+
+    @PostMapping (name ="/criar")
+    public String criarNinja(){
+        return "Criado";
+    }
+
+    @GetMapping ("/mostrarninjas")
+    public String mostarNinjas (){
+        return "Ninjas";
+    }
+
+    @GetMapping ("/mostarninjaid")
+    public String mostrarNinjaPorId (){
+        return "Ninja ID";
+    }
+    @PutMapping ("/atualizarninja")
+    public String atualizarNinja (){
+        return "Ninja Atualizado";
+    }
+
+    @DeleteMapping ("/deletarNinja")
+    public String deletarNinja(){
+        return "Ninja deletado";
     }
 }
