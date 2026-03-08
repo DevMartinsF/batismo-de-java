a40974cbfbf4ec3b12ef2e791988fdd5280a285d
DevMartinsF <r.martins.programmer@gmail.com>
2026-03-07 21:34:58 -0300

    fix: atualiza o nome das rotas do NinjaController

--- src/main/java/dev/martinsf/batismo_de_java/Ninjas/NinjaController.java
+++ src/main/java/dev/martinsf/batismo_de_java/Ninjas/NinjaController.java
@@ -3,28 +3,28 @@ package dev.martinsf.batismo_de_java.Ninjas;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping ("ninjas")
 public class NinjaController {
     @GetMapping("/boasvindas")
     public String boasVindas (){
         return "Boas Vindas";
     }
 
-    @PostMapping (name ="/criar")
+    @PostMapping (name ="/adicionarNinjas")
     public String criarNinja(){
         return "Criado";
     }
 
-    @GetMapping ("/mostrarninjas")
+    @GetMapping ("/mostrarNinjas")
     public String mostarNinjas (){
         return "Ninjas";
     }
 
-    @GetMapping ("/mostarninjaid")
+    @GetMapping ("/mostarNinjaID")
     public String mostrarNinjaPorId (){
         return "Ninja ID";
     }
-    @PutMapping ("/atualizarninja")
+    @PutMapping ("/atualizarNinja")
     public String atualizarNinja (){
         return "Ninja Atualizado";
     }
