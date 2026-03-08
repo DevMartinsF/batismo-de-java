dee4f88e0a3472f2ae30c23f11c97c81f4d1491a
DevMartinsF <r.martins.programmer@gmail.com>
2026-03-08 12:18:16 -0300

    feat: implementa a lógica dentro de MissoesService

--- src/main/java/dev/martinsf/batismo_de_java/Missoes/MissoesService.java
+++ src/main/java/dev/martinsf/batismo_de_java/Missoes/MissoesService.java
@@ -1,4 +1,18 @@
 package dev.martinsf.batismo_de_java.Missoes;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MissoesService {
+private MissoesRepository missoesRepository;
+
+    public MissoesService(MissoesRepository missoesRepository) {
+        this.missoesRepository = missoesRepository;
+    }
+
+    public List <MissoesModel> mostrarMissoes(){
+        return missoesRepository.findAll();
+    }
 }
