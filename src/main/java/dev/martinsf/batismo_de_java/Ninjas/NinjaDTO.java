9674d831816595e4a097320323ba91326e7f3fb7
DevMartinsF <r.martins.programmer@gmail.com>
2026-04-11 18:39:29 -0300

    chore: implementa a relação entidade x dto manualmente

--- src/main/java/dev/martinsf/batismo_de_java/Ninjas/NinjaDTO.java
+++ src/main/java/dev/martinsf/batismo_de_java/Ninjas/NinjaDTO.java
@@ -1,4 +1,19 @@
 package dev.martinsf.batismo_de_java.Ninjas;
 
+import dev.martinsf.batismo_de_java.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private Long id;
+    private  String nome;
+    private  int idade;
+    private  String gmail;
+    private MissoesModel missao;
+    private String rank;
 }
--- src/main/java/dev/martinsf/batismo_de_java/Ninjas/NinjaMapper.java
+++ src/main/java/dev/martinsf/batismo_de_java/Ninjas/NinjaMapper.java
@@ -1,4 +1,31 @@
 package dev.martinsf.batismo_de_java.Ninjas;
 
-public class NinjaMapper {
+import org.springframework.stereotype.Component;
+
+@Component
+public class NinjaMapper{
+
+    public NinjaModel mapper(NinjaDTO ninjaDTO) {
+
+        NinjaModel ninjaModel = new NinjaModel();
+        ninjaModel.setId(ninjaDTO.getId());
+        ninjaModel.setNome(ninjaDTO.getNome());
+        ninjaModel.setIdade(ninjaDTO.getIdade());
+        ninjaModel.setGmail(ninjaDTO.getGmail());
+        ninjaModel.setMissao(ninjaDTO.getMissao());
+        ninjaModel.setRank(ninjaDTO.getRank());
+
+        return ninjaModel;
+    }
+     public NinjaDTO mapperDTO(NinjaModel ninjaModel){
+        NinjaDTO ninjaDTO = new NinjaDTO();
+         ninjaDTO.setId(ninjaModel.getId());
+         ninjaDTO.setNome(ninjaModel.getNome());
+         ninjaDTO.setIdade(ninjaModel.getIdade());
+         ninjaDTO.setGmail(ninjaModel.getGmail());
+         ninjaDTO.setMissao(ninjaModel.getMissao());
+         ninjaDTO.setRank(ninjaModel.getRank());
+
+         return ninjaDTO;
+     }
 }
